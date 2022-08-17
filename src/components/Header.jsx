18501e68e4f0f5b8e8bf3e54bcdf139e12fcccac
leeyeji <lyj990701@gmail.com>
2022-08-18 02:55:18 +0900

    feat:create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import 'header.css';
+function Header(){
+    return (<div className="head_wrap">
+            <div>My Todo List</div>
+            <div>React</div>
+            </div>
+          );
+}
+export default Header;   
